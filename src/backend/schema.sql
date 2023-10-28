89d04db7d7d558a2603784b7113ca86220c35d38
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-10-28 01:38:13 -0400

    Add experimental SQL schema

--- src/backend/schema.sql
+++ src/backend/schema.sql
@@ -1,36 +1,64 @@
 CREATE DATABASE edugators;
-\c lab4a;
+
+\c edugators;
+
+CREATE TABLE Teacher (
+    teacherId SERIAL PRIMARY KEY,
+    name VARCHAR(128)
+);
+
+CREATE TABLE Course (
+    courseId SERIAL PRIMARY KEY
+);
+
+-- Junction table for teacher-course relationship
+CREATE TABLE TeacherCourse (
+    teacherId INTEGER REFERENCES Teacher(teacherId),
+    courseId INTEGER REFERENCES Course(courseId),
+    PRIMARY KEY (teacherId, courseId)
+);
 
 CREATE TABLE Student (
     studentId SERIAL PRIMARY KEY,
     name VARCHAR(128),
     level VARCHAR(128),
-    classes class[],
     expectedGraduationDate DATE
-)
+);
 
-CREATE TABLE Course (
-    courseId SERIAL PRIMARY KEY,
-    teacher teacher,
-    students student[],
-)
+-- Junction table for student-course relationship
+CREATE TABLE StudentCourse (
+    studentId INTEGER REFERENCES Student(studentId),
+    courseId INTEGER REFERENCES Course(courseId),
+    PRIMARY KEY (studentId, courseId)
+);
 
-CREATE TABLE Teacher (
-    teacherId SERIAL PRIMARY KEY,
-    name VARCHAR(128),
-    classes class[]
-)
+CREATE TABLE ContentArea (
+    contentAreaId SERIAL PRIMARY KEY,
+    courseId INTEGER REFERENCES Course(courseId),
+    name VARCHAR(255),
+    order_index INTEGER
+);
 
-CREATE table AssignmentGroup (
-    assignments assignment[]
-)
+CREATE TABLE Content (
+    contentId SERIAL PRIMARY KEY,
+    contentAreaId INTEGER REFERENCES ContentArea(contentAreaId),
+    title TEXT,
+    description TEXT,
+    order_index INTEGER,
+    file_path TEXT
+);
 
-CREATE table Assignment (
-    title text,
-    description text?,
-    uploadedFiles file?,
-    comletionStatus status?,
-)
+CREATE TABLE Assignment (
+    assignmentId SERIAL PRIMARY KEY,
+    courseId INTEGER REFERENCES Course(courseId),
+    title TEXT,
+    description TEXT
+);
 
--- INSERT INTO animals (name, age, species) VALUES ('Test', 77, 'test');
-
+CREATE TABLE Submission (
+    submissionId SERIAL PRIMARY KEY,
+    studentId INTEGER REFERENCES Student(studentId),
+    assignmentId INTEGER REFERENCES Assignment(assignmentId),
+    file_path TEXT,
+    submission_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
