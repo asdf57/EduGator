72a0ed161edc62fcdf5089706d1c741c7b4dde4d
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-11-02 01:21:11 -0400

    Add initial server login logic. Add postgres setup logic.

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,71 +1,84 @@
-let express = require("express");
-let path = require("path");
+const express = require("express");
+const pg = require("pg");
+const path = require("path");
+const session = require("express-session")
 
-const hostname = "0.0.0.0";
-const port = process.env.PORT;
+
 const app = express();
-
-const SESSIONS = {};
-const SESSION_DURATION = 3600000;
-
-// Middleware Handlers
-app.use("/scripts", express.static(path.join(__dirname, "..", "frontend", "scripts")));
-app.use("/dist", express.static(path.join(__dirname, "..", "frontend", "dist")));
-app.use("/assets", express.static(path.join(__dirname, "..", "frontend", "assets")));
-app.use(express.static(path.join(__dirname, "..", "frontend", "public")));
-
+const SESSION_DURATION = 60000;
+const HOSTNAME = "0.0.0.0";
+const PORT = process.env.PORT;
+const ROLES = ["admin", "teacher", "student"];
 const LoginType = {
 	Student: "student",
 	Teacher: "teacher",
 	Admin: "admin",
-}
+  Unknown: "unknown"
+};
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL
+});
 
+pool.connect();
 
-// function getLoginType() {
-//   return ...
-// }
-
-// Global login session middleware
+app.use(session({
+  secret: process.env.SIGNING_KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: SESSION_DURATION }
+}));
+app.use(express.json());
+app.use("/scripts", express.static(path.join(__dirname, "..", "frontend", "scripts")));
+app.use("/dist", express.static(path.join(__dirname, "..", "frontend", "dist")));
+app.use("/assets", express.static(path.join(__dirname, "..", "frontend", "assets")));
+app.use(express.static(path.join(__dirname, "..", "frontend", "public")));
 app.use((req, res, next) => {
-  if (isUserAuthenticated(req.cookies)) {
-    const sessionId = generateUniqueSessionId();
-    SESSIONS[sessionId] = {
-        user: username,
-        lastAccess: Date.now()
-    };
+  console.log('Global middleware session:', req.session);
+  console.log("Authed: ", req.session.isAuthenticated);
 
-    res.cookie('sessionId', sessionId);
-    res.redirect('/home');
-  } else {
-    console.log("Not authed!");
-    res.redirect('/index.html');
-  }
+  if (req.session.isAuthenticated)
+    return next();
+  
+  if (req.path === '/login' || req.path === '/login.html')
+    return next();
 
-  next();
+  console.log("Not authed!");
+  res.redirect('/login.html');
 });
 
-// Checks to see if a valid session token exists and isn't expired
-function isUserAuthenticated(cookies) {
-  return cookies &&
-         cookies.sessionId &&
-         SESSIONS[sessionId] &&
-         (Date.now() - SESSIONS[sessionId].lastAccess) < SESSION_DURATION;
+app.use((err, req, res, next) => {
+  return res.status(400).end();
+});
+
+function getUserType(req) {
+  if (req.session.role == "student")
+    return LoginType.Student;
+  else if (req.session.role == "teacher")
+    return LoginType.Teacher;
+  else if (req.session.role == "admin")
+    return LoginType.Admin;
+
+  return LoginType.Unknown;
 }
 
-app.post("/login", (req, res) => {
-  if (isUserAuthenticated()) {
-    const sessionId = generateUniqueSessionId();
-    SESSIONS[sessionId] = {
-        user: username,
-        lastAccess: Date.now()
-    };
-    res.cookie('sessionId', sessionId);
-    res.redirect('/');
+app.get("/home", (req, res) => {
+  const userType = req.session.role;
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password, role } = req.body;
+
+  console.log("Received: ", req.body, req.headers);
+
+  if (req.session.isAuthenticated || username === "test") {
+    req.session.isAuthenticated = true;
+    req.session.role = role;
+    return res.redirect('/home.html');
   } else {
-      res.redirect('/login');
+    return res.status(401).json({"error": "Unauthorized"});
   }
 });
 
-app.listen(port, hostname, () => {
-  console.log(`http://${hostname}:${port}`);
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`http://${HOSTNAME}:${PORT}`);
 });
