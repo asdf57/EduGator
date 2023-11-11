dc734c1ebc61b22cc433502dbf0a35bb8cc04523
asdf57 <53836417+asdf57@users.noreply.github.com>
2023-11-11 14:07:16 -0500

    Remove backend utils directory

--- src/backend/utils/courses.js
+++
@@ -1,9 +0,0 @@
-const pg = require("pg");
-
-async function getAllCourses(pool) {
-    try {
-        const coursesQuery = await pool.query(`SELECT * FROM courses`);
-    } catch (error) {
-
-    }
-}
